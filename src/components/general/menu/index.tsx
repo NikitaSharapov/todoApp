aee3ca3ed4d7d21a52f178ff6e877019348ee978
Nikita Shrarapov <nikitasharapov147@gmail.con>
2021-03-30 22:36:32 +0500

    add menu item

--- src/components/general/menu/index.tsx
+++ src/components/general/menu/index.tsx
@@ -3,6 +3,7 @@ import { Link, useHistory } from 'react-router-dom';
 import Popup from 'reactjs-popup';
 import styled, { keyframes } from 'styled-components';
 import { AuthContext } from '../../../context/authContext';
+import { ModalVoiceInfo } from '../../app/modalVoiceInfo';
 import { PhoneTodoAddForm } from '../../app/todo';
 
 const anim = keyframes`
@@ -22,7 +23,6 @@ export const MenyContainer = styled.div`
   background: #000;
   position: fixed;
   display: grid;
-  grid-template: 1fr 1fr 1fr/ 150px;
   border-radius: 10px 0 10px 10px;
   row-gap: 15px;
   padding: 15px;
@@ -35,6 +35,7 @@ export const MenyItem = styled.button`
   border: none;
   background: none;
   outline: none;
+  text-align: start;
 `
 
 export const MobileMenu: React.FC = () => {
@@ -50,8 +51,13 @@ export const MobileMenu: React.FC = () => {
   return (
     <MenyContainer>
       <MenyItem onClick={()=>{link()}} >Главная</MenyItem>
-      <Popup trigger={<div><MenyItem>Создать задачу</MenyItem></div>} position={['top center']} closeOnDocumentClick>
+      <Popup modal trigger={<div><MenyItem>Создать задачу</MenyItem></div>} position={['top center']} closeOnDocumentClick>
         <PhoneTodoAddForm />
+      </Popup>
+      <Popup modal trigger={<div><MenyItem>Голосовое управление</MenyItem></div>} position={['top center']} closeOnDocumentClick overlayStyle={{background: 'rgba(0,0,0,.4'}}>
+      {(close: any) => (
+        <ModalVoiceInfo close={()=>close()}/>
+        )}
       </Popup>
       <MenyItem onClick={()=>{logoutHandler()}}>Выйти</MenyItem>
     </MenyContainer>
